66dbedbce3a9c75f5bba3415f1d7185761932eb2
adritoo <adrien.bourget16@laposte.net>
2020-12-23 22:48:40 +0100

    Implement home button and dynamic map on /map

---
+++ src/Main.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'reactstrap';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Main extends Component {
+ 
+  render(){
+    return (
+              <Button color="warning"><Link to="/map">Go to the map</Link></Button>
+    );
+  }
+}
+
+
+export default Main;
