e61677fbd79961134e75430d9edd3efcfa4204c6
adritoo <adrien.bourget16@laposte.net>
2020-12-31 10:30:42 +0100

    Issue #2, installed react-helmet

--- src/Main.js
+++ src/Main.js
@@ -1,17 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-import { Button } from 'reactstrap';
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import "./App.css";
+import { Button } from "reactstrap";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 class Main extends Component {
- 
-  render(){
+  render() {
     return (
-              <Button color="warning"><Link to="/map">Go to the map</Link></Button>
+      <>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>Ortheos</title>
+          <link rel="canonical" href="http://mysite.com/example" />
+        </Helmet>
+        <Button color="warning">
+          <Link to="/map">Go to the map</Link>
+        </Button>
+      </>
     );
   }
 }
-
 
 export default Main;
