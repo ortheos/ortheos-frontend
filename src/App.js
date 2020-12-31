fd3e8632270ee325a717ccdb8bb1f9a06e321faf
adritoo <adrien.bourget16@laposte.net>
2020-12-31 10:06:19 +0100

    Solving issue #4

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Map from "./components/Map.js";
 import Main from "./pages/Main.js";
 
+
 class App extends Component {
   render() {
     return (
